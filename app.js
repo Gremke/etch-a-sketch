0652607dec21ee317dcb012fb21dcfe6b2b60f0a
Christian Thornton <gremke@gmail.com>
2024-01-12 17:13:30 -0800

    Refactor grid logic. Use css grid instead of flexbox

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-// Create a webpage with a 16x16 grid of square divs.
+const gridContainer = document.getElementById('gridContainer');
+const columns = 30;
+const rows = 30;
+const gridItem = document.createElement('div');
+const gridItems = document.querySelectorAll('.gridItem');
+
+
+const setGrid = (columns, rows) => {
+  gridContainer.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${rows}, 1fr)`; // Adjust height as needed
+};
 
 const newGrid = () => {
-  let box = document.createElement('div');
-  box.className = "box"
-  return box;
+  let cell = document.createElement('div');
+  cell.className = "cell"
+  return cell;
 };
 
-const gridContainer = document.querySelector('.grid-container');
-for (i = 0; i < 1599; i++) {
-  const div = newGrid();
-  gridContainer.appendChild(div);
-};
+for (i = 0; i < columns * rows; i++) {
+  gridItem.classList.add('gridItem');
+  const gridCell = newGrid();
+  gridContainer.appendChild(gridCell);
+}
 
-const boxes = document.querySelectorAll('.box');
-boxes.forEach((box) => {
-  box.addEventListener('mouseover', () => {
-    if (box.classList.contains('colored')) {
-      box.classList.remove('colored')
+gridItems.forEach((gridItem) => {
+  gridItem.addEventListener('mouseover', () => {
+    if (gridItem.classList.contains('colored')) {
+      gridItem.classList.remove('colored')
     } else {
-      box.classList.add('colored')
+      gridItem.classList.add('colored')
     }
   });
 });
+
+setGrid(columns, rows);
